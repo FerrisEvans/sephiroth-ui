2242a25c5fa7d623bf6bbba13a2283b4adc564f6
Ferris <ferris.evans@outlook.com>
2024-06-06 17:08:40 +0800

    i18n

---
+++ src/locales/index.ts
@@ -0,0 +1,10 @@
+import {I18n} from "@/@types/i18n";
+import zhCN from "@/locales/zh-CN.ts";
+import enUS from "@/locales/en-US.ts";
+
+const locales: I18n = {
+  'zh-CN': zhCN,
+  'en-US': enUS,
+}
+
+export default locales
