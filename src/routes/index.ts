239308b65549807c2cf0e8b37526e9dd55b42d77
Ferris <ferris.evans@outlook.com>
2024-06-09 23:03:15 +0800

    route config

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import React, { lazy, memo, Suspense } from 'react';
-import { useLocation, useRoutes } from 'react-router-dom';
-import { createUseRoutes, pathKeyCreateUseRoutes } from '@/utils/router';
-
-const routes = [
-
-]
