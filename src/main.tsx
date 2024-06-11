ca312d9844c76514d9b82f38d501204b1d7b2d49
Ferris <ferris.evans@outlook.com>
2024-06-11 14:20:09 +0800

    react palette

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import 'reset-css'
 import './index.scss'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "@/dev";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <DevSupport ComponentPreviews={ComponentPreviews}
+                  useInitialHook={useInitial}
+      >
+        <App/>
+      </DevSupport>
     </BrowserRouter>
   </React.StrictMode>,
 )
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
+import i18nReducer from "@/stores/i18n";
 
 const store = configureStore({
   reducer: {
-
+    i18n: i18nReducer
   }
 })
 
