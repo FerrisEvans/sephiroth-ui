14db46ed93c9b15ff547f13de6f6dfd3aab66102
Ferris <ferris.evans@outlook.com>
2024-06-13 00:22:01 +0800

    i18n

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,17 @@ import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "@/dev";
+import {RecoilRoot} from "recoil";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <DevSupport ComponentPreviews={ComponentPreviews}
-                  useInitialHook={useInitial}
-      >
-        <App/>
-      </DevSupport>
+      <RecoilRoot>
+        <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+          <App/>
+        </DevSupport>
+      </RecoilRoot>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  ,
 )
