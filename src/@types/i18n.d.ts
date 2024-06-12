909c6498e465b3bbcb9b50a90c2ef97c51e6a37d
Ferris <ferris.evans@outlook.com>
2024-06-12 23:53:43 +0800

    i18n type

--- src/@types/i18n.d.ts
+++ src/@types/i18n.d.ts
@@ -7,3 +7,19 @@ export interface I18nVal {
 export type I18n = {
   [key in I18nkey]?: I18nVal
 }
+
+type Lang = {
+  label: string,
+  value: I18nKey
+}[]
+
+export const lang: Lang = [
+  {
+    label: '简体中文',
+    value: 'zh-CN'
+  },
+  {
+    label: 'English',
+    value: 'en-US'
+  }
+]
