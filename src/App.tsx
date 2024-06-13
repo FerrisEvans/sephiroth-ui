697eadec143bd029a9b4190cf455fa0271ecf1e6
Ferris <ferris.evans@outlook.com>
2024-06-13 23:07:17 +0800

    build successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import Routes from "@/routes";
-import {memo, useEffect} from "react";
-import {useRecoilValue} from "recoil";
-import {antdMessageState, i18nLocaleState} from "@/stores/i18n.ts";
-import {setHtmlLang} from "@/utils/i18n.ts";
-import {ConfigProvider} from "antd";
+import {memo} from "react"
 
 export default memo(() => {
-  const i18nLocale = useRecoilValue(i18nLocaleState)
-  const antdMessage = useRecoilValue(antdMessageState)
-
-  useEffect(() => {
-    setHtmlLang(i18nLocale);
-  }, [])
 
   return (
     <>
-      <ConfigProvider locale={antdMessage}>
-        <Routes />
-      </ConfigProvider>
+
     </>
   )
 })
