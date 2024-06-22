2efc9e35341c8839ec483be64ca2600aa45121ca
Ferris <ferris.evans@outlook.com>
2024-06-23 00:02:23 +0800

    api client

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,67 +1,81 @@
+import {message as Message} from 'antd'
 import axios, {AxiosRequestConfig, AxiosError, AxiosResponse} from 'axios'
-import {Result} from '@/types/axios.ts'
+import {isEmpty} from 'ramda'
+
+import {t} from '@/locales/i18n'
+
+import {Result} from '@/types/api'
+import {ResultEnum} from '@/types/enum'
 
 // 创建 axios 实例
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_API as string,
+  baseURL: import.meta.env.VITE_APP_BASE_API,
   timeout: 50000,
-  headers: {'Content-Type': 'application/json;charset=utf-8'},
+  headers: {'Content-Type': 'application/jsoncharset=utf-8'},
 })
 
 // 请求拦截
 axiosInstance.interceptors.request.use(
   (config) => {
     // 在请求被发送之前做些什么
-    console.log(config);
-
-    config.headers.Authorization = 'Bearer Token';
-    return config;
+    config.headers.Authorization = 'Bearer Token'
+    return config
   },
   (error) => {
     // 请求错误时做些什么
-    return Promise.reject(error);
+    return Promise.reject(error)
   },
 )
 
 // 响应拦截
 axiosInstance.interceptors.response.use(
-  (response: AxiosResponse) => {
-    // 对响应数据做点什么
-    return response.data;
+  (res: AxiosResponse<Result>) => {
+    if (!res.data) throw new Error(t('sys.api.apiRequestFailed'))
+
+    const {status, data, message} = res.data
+    // 业务请求成功
+    const hasSuccess = data && Reflect.has(res.data, 'status') && status === ResultEnum.SUCCESS
+    if (hasSuccess) {
+      return data
+    }
+
+    // 业务请求错误
+    throw new Error(message || t('sys.api.apiRequestFailed'))
   },
-  (error: AxiosError) => {
+  (error: AxiosError<Result>) => {
+    const {response, message} = error || {}
+    let errMsg = ''
+    try {
+      errMsg = response?.data?.message || message
+    } catch (error) {
+      throw new Error(error as unknown as string)
+    }
     // 对响应错误做点什么
-    return Promise.reject(error);
+    if (isEmpty(errMsg)) {
+      // checkStatus
+      // errMsg = checkStatus(response.data.status)
+      errMsg = t('sys.api.errorMessage')
+    }
+    Message.error(errMsg).then(r => console.error(r))
+    return Promise.reject(error)
   },
 )
 
 class ApiClient {
   get<T = any>(config: AxiosRequestConfig): Promise<T> {
-    return this.request({
-      ...config,
-      method: 'GET'
-    })
+    return this.request({...config, method: 'GET'})
   }
 
   post<T = any>(config: AxiosRequestConfig): Promise<T> {
-    return this.request({
-      ...config,
-      method: 'POST'
-    })
+    return this.request({...config, method: 'POST'})
   }
 
   put<T = any>(config: AxiosRequestConfig): Promise<T> {
-    return this.request({
-      ...config,
-      method: 'PUT'
-    })
+    return this.request({...config, method: 'PUT'})
   }
 
   delete<T = any>(config: AxiosRequestConfig): Promise<T> {
-    return this.request({
-      ...config,
-      method: 'DELETE'
-    })
+    return this.request({...config, method: 'DELETE'})
   }
 
   request<T = any>(config: AxiosRequestConfig): Promise<T> {
